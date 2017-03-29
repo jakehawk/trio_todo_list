ff084367e6102416856902cc9f14dd7a25b62aa6
Jake Hawkinson <jakehawk1095@gmail.com>
2017-03-29 11:58:22 -0700

    Created seed data

--- js/todosController.js
+++ js/todosController.js
@@ -5,7 +5,23 @@
     .controller(`TodosApp`, TodosApp)
 
   function TodosApp($scope) {
-
+  	$scope.allTodos = [
+  		{
+  			id 					: 0,
+  			task 				: "Code a lot",
+  			completed 	: false
+  		},
+  		{
+  			id					: 1,
+  			task				: "Complete task one",
+  			completed		: false
+  		},
+  		{
+  			id					: 2,
+  			task				: "get stuff to show up",
+  			completed		: false
+  		}
+  	]
   }
 
 }())
